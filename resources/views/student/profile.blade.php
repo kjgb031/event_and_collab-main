b99552bba19affece8f09bcbaae78b5ef4177fc9
John Edmerson Pizarra <edmerpizarra@gmail.com>
2024-11-16 14:26:02 +0800

    feat: add profile link to navigation and back button on student profile page

--- resources/views/student/profile.blade.php
+++ resources/views/student/profile.blade.php
@@ -8,6 +8,9 @@
 <div class="container mx-auto">
     <div class="p-5 my-10 bg-white rounded-lg shadow-md">
         <div>
+            <div class="flex items-center justify-end">
+                <a href="{{ route('student.dashboard') }}" class="mb-4 btn btn-primary">Back</a>
+            </div>
             <livewire:student-edit-form />
         </div>
     </div>
